ba98ad7b48ad10e46fd03492f188cebb07309b42
caleb <calebburke91@gmail.com>
2024-05-29 14:28:51 -0700

    Testing api calls

--- api/server.js
+++ api/server.js
@@ -9,16 +9,20 @@ const PORT = 5000;
 
 app.post("/", async (req, res) => {
     console.log("POST recv");
-    
+    /*
     if (req.files) {
         let files = Array.isArray(req.files.files) ? req.files.files : [req.files.files];
         let newFile = await topdfStream(files[0]);
         res.sendFile(newFile);
     } else {
         console.log("Error?");
-    }
+    }*/
     
     res.status(200).send("hello");
+});
+
+app.get("/", (req, res) => {
+    res.sendStatus(200);
 });
 
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
